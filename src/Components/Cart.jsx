80759bf1af39a42b763b96e7a4bcb0bf0e4eb0b4
Chaitanya <ckulkarni120@gmail.com>
2020-05-23 01:38:33 +0530

    responsiveness

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -34,28 +34,6 @@ export class Cart extends Component {
                         : ""
                     }
                     {this.props.cart.map((e, i) => {
-                        // return <div className="card mb-3 col-lg-8 col-xs-12" key={i}>
-                        //     <div className="row no-gutters">
-                        //         <div className="col-lg-4 col-xs-12 px-md-5 pt-5 pb-3">
-                        //             <img src={e.productImage} className="card-img" alt="..." />
-                        //         </div>
-                        //         <div className="col-lg-8 col-xs-12">
-                        //             <div className="card-body">
-                        //                 <h5 className="card-title">{e.productName}</h5>
-                        //             </div>
-                        //             <span className="text-muted px-3"><strong>{e.productBrand}</strong></span>
-                        //             <span className="badge bg-success rounded text-light">{e.productRating} <small><i className="fa fa-star" aria-hidden="true"></i></small></span>
-                        //             <span>
-                        //                 <span className="text-muted px-3">Color: {e.productColor}</span>
-                        //             </span>
-                        //             <div className="float-lg-right">
-                        //                 <span className="pr-2 "><strong><i className="fa fa-inr" aria-hidden="true"></i> {e.productDiscountPrice}</strong></span>
-                        //                 <span className="text-muted pr-2"><strike><i className="fa fa-inr" aria-hidden="true"></i> {e.productOriginalPrice}</strike></span>
-                        //                 <span className="text-success ">{((e.productOriginalPrice - e.productDiscountPrice) / e.productOriginalPrice).toFixed(2) * 100}% off</span>
-                        //             </div>
-                        //         </div>
-                        //     </div>
-                        // </div>
                         return <div class="card mb-3 m-3 px-3 pt-3" style={{ "max-width": "640px" }}>
                             <div class="row no-gutters">
                                 <div class="col-md-4">
